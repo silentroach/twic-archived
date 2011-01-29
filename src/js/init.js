c04e78896d5f24ec5d1cab42203147d1e0b77b1b
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-01-29 22:54:25 +0300

    oauth?

--- src/js/init.js
+++ src/js/init.js
@@ -1,3 +1,52 @@
 var twic = {
-	name: 'Twic'
+	name: 'Twic',
+	oauth: {	
+		consumer_key: 'Yda6L1lsEkqwDhcqxWPXtw',
+		consumer_secret: 'IHtRC1kPwQ4MH1lccSaZGdhZPyPiw2iuEfhCDV4'
+	}
 };
+
+
+/* f*ckin oauth
+
+    nonce: function {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var result = "";
+		
+        for (var i = 0; i < 6; ++i) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            result += chars.substring(rnum, rnum+1);
+        }
+		
+        return result;
+    }
+	
+	        var baseString = OAuth.SignatureMethod.getBaseString(message);
+        var signature = this.getSignature(baseString);
+		
+    getBaseString: function getBaseString(message) {
+        var URL = message.action;
+        var q = URL.indexOf('?');
+        var parameters;
+        if (q < 0) {
+            parameters = message.parameters;
+        } else {
+            // Combine the URL query string with the other parameters:
+            parameters = OAuth.decodeForm(URL.substring(q + 1));
+            var toAdd = OAuth.getParameterList(message.parameters);
+            for (var a = 0; a < toAdd.length; ++a) {
+                parameters.push(toAdd[a]);
+            }
+        }
+        return OAuth.percentEncode(message.method.toUpperCase())
+         +'&'+ OAuth.percentEncode(OAuth.SignatureMethod.normalizeUrl(URL))
+         +'&'+ OAuth.percentEncode(OAuth.SignatureMethod.normalizeParameters(parameters));
+    }		
+	
+	b64pad = '='
+	b64_hmac_sha1(key, baseString)
+	
+	
+        this.key = OAuth.percentEncode(consumerSecret)
+             +"&"+ OAuth.percentEncode(accessor.tokenSecret);
+*/
