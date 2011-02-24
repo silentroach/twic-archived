826d3b24a872dd4c798b9552a793d5752d39db07
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-02-24 22:36:30 +0300

    tweets

--- src/js/background/db.js
+++ src/js/background/db.js
@@ -9,6 +9,7 @@ twic.db = ( function() {
 		'0': {
 			version: '0.01',
 			callback: function(t) {
+				// users info
 				t.executeSql('create table users (' + 
 					'id int not null primary key, ' +
 					'name text not null, ' +
@@ -16,12 +17,26 @@ twic.db = ( function() {
 					'avatar text not null, ' + 
 					'url text not null, ' + 
 					'verified int not null, ' +
-					'dt int not null) ');
+					'dt int not null)');
 					
+				// twic accounts
 				t.executeSql('create table accounts (' +
 					'id int not null primary key, ' +
 					'oauth_token text not null, ' +
 					'oauth_token_secret text not null)');
+
+				// tweets storage
+				t.executeSql('create table tweets (' +
+					'id int not null primary key, ' +
+					'user_id int not null, ' +
+					'reply_to int null, ' +
+					'dt int not null, ' +
+					'msg text not null)');
+
+				// timeline table for each account
+				t.executeSql('create table timeline (' +
+					'user_id int not null, ' +
+					'tweet_id int not null)');
 			}
 		}
 	};
@@ -30,8 +45,9 @@ twic.db = ( function() {
 	 * Migration procedure
 	 * @param {Database} db Database
 	 * @param {string} ver Source database version
+	 * @param {function()} callback Callback function
 	 */
-	var migrate = function(db, ver) {
+	var migrate = function(db, ver, callback) {
 		var version = (ver == '') ? '0' : ver;
 
 		if (version in migrations) {
@@ -42,7 +58,17 @@ twic.db = ( function() {
 			}, function() {
 				migrate(db, migration.version);							
 			} );
+		} else {
+			callback();
 		}
+	};
+	
+	/**
+	 * Cleanup the database
+	 * @param {Database} db Database
+	 */
+	var cleanup = function(db) {
+	
 	};
 	
 	var database = null;
@@ -50,7 +76,9 @@ twic.db = ( function() {
 	var getDatabase = function() {
 		if (!database) {
 			database = openDatabase(twic.name, '', twic.name, null);
-			migrate(database, database.version);
+			migrate(database, database.version, function() {
+				cleanup(database);
+			} );
 		}
 		
 		return database;
