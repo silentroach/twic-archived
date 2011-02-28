d3019e7395b3ad3b42e6371c33c384ddbda07b71
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-02-28 19:51:20 +0300

    timeline makeup

--- src/js/popup/timeline.js
+++ src/js/popup/timeline.js
@@ -5,59 +5,63 @@
 
 ( function() {
 
-  var
-	  /** @type {HTMLUListElement} */ list = document.querySelector('#timeline ul');
+	var
+		/** @type {HTMLUListElement} */ list = document.querySelector('#timeline ul');
 
-  var buildList = function(data) {
-	console.dir(data);
-  
-	var frag = document.createDocumentFragment();
-		
-	for (var id in data) {
-		var 
-			item      = data[id],
-			li        = document.createElement('li'),
-			avatarEl  = document.createElement('img'),
-			nickEl    = document.createElement('p'),
-			messageEl = document.createElement('p');
+	var buildList = function(data) {
+		console.dir(data);
 
-		avatarEl.src        = item['user']['avatar'];
-		avatarEl.className  = 'avatar';
-		
-		nickEl.innerHTML    = '@' + item['user']['name'];
-		messageEl.innerHTML = item['msg'];
+		var frag = document.createDocumentFragment();
 
-		li.appendChild(avatarEl);
-		li.appendChild(nickEl);
-		li.appendChild(messageEl);
+		for (var id in data) {
+			var 
+				item      = data[id],
+				li        = document.createElement('li'),
+				avatarEl  = document.createElement('img'),
+				nickEl    = document.createElement('p'),
+				messageEl = document.createElement('p'),
+				clearEl   = document.createElement('div');
 
-		frag.appendChild(li);
-	}
-		
-	list.appendChild(frag);
-  };
+			avatarEl.src        = item['user']['avatar'];
+			avatarEl.className  = 'avatar';
+			
+			nickEl.innerHTML    = '@' + item['user']['name'];
+			messageEl.innerHTML = item['msg'];
+			
+			clearEl.className = 'clearer';
 
-  twic.router.handle('timeline', function(data) {
-	  if (
-		  !data.length
-		  || 1 !== data.length
-	  ) {
-	  	// todo return to the accounts list screen
-		  return;
-	  }
-	  
-	  // make popup 60% of screen height
-	  document.body.style.height = Math.round(screen.height / 100 * 60) + 'px';
+			li.appendChild(avatarEl);
+			li.appendChild(nickEl);
+			li.appendChild(messageEl);
+			li.appendChild(clearEl);
 
-	  var id = data[0];
+			frag.appendChild(li);
+		}
 
-	  twic.requests.send('getTimeline', {
-		  'id': id
-	  }, function(list) {
-		  if (list) {
-			  buildList(list);
-		  }
-	  } );
-  } );
+		list.appendChild(frag);
+	};
+
+	twic.router.handle('timeline', function(data) {
+		if (
+			!data.length
+			|| 1 !== data.length
+		) {
+			// todo return to the accounts list screen
+			return;
+		}
+
+		// make popup 60% of screen height
+		document.body.style.height = Math.round(screen.height / 100 * 60) + 'px';
+
+		var id = data[0];
+
+		twic.requests.send('getTimeline', {
+			'id': id
+		}, function(list) {
+			if (list) {
+				buildList(list);
+			}
+		} );
+	} );
 
 } )();
