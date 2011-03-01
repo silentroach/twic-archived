ae2dc318632dbf63f78244ade832e3b3d9823556
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-03-01 23:53:36 +0300

    simple about page

---
+++ src/js/popup/about.js
@@ -0,0 +1,16 @@
+/**
+ * Kalashnikov Igor <igor.kalashnikov@gmail.com>
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ */
+
+( function() {
+
+	var
+		/** @type {HTMLElement} */ about = document.querySelector('#about'),
+		/** @type {HTMLElement} */ awhat = document.querySelector('#awhat');
+		
+	twic.router.handle('about', function(data) {
+		awhat.innerHTML = chrome.i18n.getMessage('about_what');
+	} );
+
+} )();
