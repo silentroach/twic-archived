58c0a39a5ad98554d227f5855567b97b5659ff71
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-03-10 19:12:07 +0300

    trailing spaces

--- src/js/popup/vcl/Timeline.js
+++ src/js/popup/vcl/Timeline.js
@@ -11,18 +11,18 @@ twic.vcl.Timeline = function(parent) {
 	var
 		wrapper = document.createElement('ul'),
 		tweets  = [];
-		
+
 	wrapper.className = 'timeline';
 
 	parent.appendChild(wrapper);
-	
+
 	/**
 	 * @param {!twic.vcl.Tweet} tweet Tweet
 	 */
 	var addTweet = function(tweet) {
 		wrapper.appendChild(tweet.getElement());
 	};
-	
+
 	return {
 		addTweet: addTweet
 	};
