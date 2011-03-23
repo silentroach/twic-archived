5d860d4d8f4e2dedfcf65b53b01cecce8cd3bf4f
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-03-23 23:59:15 +0300

    annotations and little improvements to db.obj.User

--- src/js/background/objects/user.js
+++ src/js/background/objects/user.js
@@ -1,4 +1,6 @@
 /**
+ * User DB object
+ *
  * Kalashnikov Igor <igor.kalashnikov@gmail.com>
  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  */
@@ -8,10 +10,12 @@
  * @extends twic.DBObject
  */
 twic.db.obj.User = function() {
-	twic.DBObject.call(this);
+	var self = this;
 
-	this.table = 'users';
-	this.fields = {
+	twic.DBObject.call(self);
+
+	self.table = 'users';
+	self.fields = {
 		'id': 0,
 		'name': '',
 		'screen_name': '',
@@ -25,7 +29,7 @@ twic.db.obj.User = function() {
 		'dt': twic.utils.date.getCurrentTimestamp()
 	};
 
-	this.jsonMap = {
+	self.jsonMap = {
 		'avatar': 'profile_image_url',
 		'verified': function(obj) {
 			return (obj['verified']) ? 1 : 0;
@@ -42,10 +46,16 @@ twic.db.obj.User = function() {
 
 goog.inherits(twic.db.obj.User, twic.DBObject);
 
-// todo annotation is missing
+/**
+ * Overriden save method to update the dt fiels (user info last update time)
+ * @param {function()} callback Callback function
+ */
 twic.db.obj.User.prototype.save = function(callback) {
-	// update for 'dt' each save method call
-	this.fields['dt'] = twic.utils.date.getCurrentTimestamp();
+	var
+		self = this;
 
-	twic.DBObject.prototype.save.call(this, callback);
+	self.fields['dt'] = twic.utils.date.getCurrentTimestamp();
+
+	twic.DBObject.prototype.save.call(self, callback);
 };
+
