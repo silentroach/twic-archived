ede90289c56e8f4b6bfe74f238fd26f755f7735a
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-03-24 00:00:54 +0300

    escaping '-' sign in vcl.Tweet::urlSearchPattern, thanks linter

--- src/js/popup/vcl/Tweet.js
+++ src/js/popup/vcl/Tweet.js
@@ -15,7 +15,7 @@ twic.vcl.Tweet = function(timeline) {
 		 * http://daringfireball.net/2010/07/improved_regex_for_matching_urls
 		 * @type {RegExp}
 		 */
-		urlSearchPattern    = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi,
+		urlSearchPattern    = /\b((?:[a-z][\w\-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi,
 
 		/** @type {RegExp} */ nickSearchPattern   = /[\@]+(\w+)/gi,
 		/** @type {RegExp} */ hashSearchPattern   = /[\#]+(\w+)/gi,
