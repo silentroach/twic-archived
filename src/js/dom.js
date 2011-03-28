46bf797cba660dc25874f837f43d938186462183
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-03-28 22:45:02 +0400

    oh

---
+++ src/js/dom.js
@@ -0,0 +1,58 @@
+/**
+ * Kalashnikov Igor <igor.kalashnikov@gmail.com>
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ */
+
+/**
+ * Some DOM utils
+ */
+
+twic.dom = {
+	/** @const **/ idExpr: /^#([^ .>:]+)$/,
+	/** @const **/ expExpr: /((^|#|\.)\w+)/g,
+};
+
+/**
+ * Find element
+ * @param {string} selector Selector
+ * @param {Element=} context Context
+ * @return {?Element}
+ */
+twic.dom.find = function(selector, context) {
+	var
+		matches               = twic.dom.idExpr.exec(selector),
+		doc                   = (matches || !context) ? document : context;
+
+	return matches ? doc.getElementById(matches[1]) : doc.querySelector(selector);
+};
+
+/**
+ * Expand the expression
+ * @param {string} expr Expression
+ * @return {Element}
+ */
+twic.dom.expand = function(expr) {
+	var
+		element,
+		res;
+
+	// fixme it smells
+	
+	res = twic.dom.expExpr.exec(expr);
+
+	while (res) {
+		if (res[2] === '') {
+			element = document.createElement(res[1]);
+		} else
+		if (res[2] === '.') {
+			element.classList.add(res[1].substring(1));
+		} else
+		if (res[2] === '#') {
+			element.setAttribute('id', res[1].substring(1));
+		}
+		
+		res = twic.dom.expExpr.exec(expr);
+	}
+
+	return element;
+};
