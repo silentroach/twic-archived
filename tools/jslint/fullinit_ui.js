f533e00311a59e6765b15bd73b771c8fb2d3bea3
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-03-28 21:03:10 +0400

    lint update

--- tools/jslint/fullinit_ui.js
+++ tools/jslint/fullinit_ui.js
@@ -1,5 +1,5 @@
 // init_ui.js
-// 2011-02-28
+// 2011-03-13
 
 // This is the web browser companion to fulljslint.js. It is an ADsafe
 // lib file that implements a web ui by adding behavior to the widget's
@@ -10,7 +10,7 @@
 
 // option = {adsafe: true, fragment: false}
 
-/*members check, cookie, each, edition, get, getCheck, getTitle,
+/*properties check, cookie, each, edition, get, getCheck, getTitle,
     getValue, has, indent, isArray, join, jslint, length, lib, maxerr, maxlen,
     on, predef, push, q, select, set, split, stringify, target, tree, value
 */
@@ -134,7 +134,7 @@ ADSAFE.lib("init_ui", function (lib) {
             output.value('Tree:');
             tree.value(JSON.stringify(lib.tree(), [
                 'label', 'value', 'arity', 'name', 'first', 'second',
-                'third', 'block', 'else', 'comments', 'postcomments'
+                'third', 'block', 'else', 'comments', 'comment', 'quote'
             ], 4));
             input.select();
         });
