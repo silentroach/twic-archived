663d5b3c4d95d5c51627779103d457bfe5372d77
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-03-31 16:07:31 +0400

    lint

--- src/js/utils.js
+++ src/js/utils.js
@@ -81,9 +81,10 @@ twic.utils.url = { };
 twic.utils.url.humanize = function(url) {
 	var
 		// cut the protocol
-		cutted = url.replace(/^(.*?)\/\//, ''),
-		// cut the first 'www.'
-		cutted = cutted.replace(/^www\./, '');
+		cutted = url.replace(/^(.*?)\/\//, '');
+
+	// cut the first 'www.'
+	cutted = cutted.replace(/^www\./, '');
 
 	if (cutted.length > 30) {
 		cutted = cutted.substring(0, 30) + '&hellip;';
