9459c3250fef8b10f10f150da0ea53c3eab9e692
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-04-04 18:13:40 +0400

    some useless incapsulation removed

--- src/js/background/handlers/profile.js
+++ src/js/background/handlers/profile.js
@@ -5,30 +5,26 @@
  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  */
 
-( function() {
-
-	twic.requests.subscribe('follow', function(data, sendResponse) {
-		twic.twitter.follow(data['id'], data['whom_id'], function() {
-			sendResponse( { } );
-		} );
+twic.requests.subscribe('follow', function(data, sendResponse) {
+	twic.twitter.follow(data['id'], data['whom_id'], function() {
+		sendResponse( { } );
 	} );
+} );
 
-	twic.requests.subscribe('unfollow', function(data, sendResponse) {
-		twic.twitter.unfollow(data['id'], data['whom_id'], function() {
-			sendResponse( { } );
-		} );
+twic.requests.subscribe('unfollow', function(data, sendResponse) {
+	twic.twitter.unfollow(data['id'], data['whom_id'], function() {
+		sendResponse( { } );
 	} );
+} );
 
-	twic.requests.subscribe('getProfileInfo', function(data, sendResponse) {
-		twic.twitter.getUserInfo( data['name'], function(user) {
-			sendResponse( user.fields );
-		} );
+twic.requests.subscribe('getProfileInfo', function(data, sendResponse) {
+	twic.twitter.getUserInfo( data['name'], function(user) {
+		sendResponse( user.fields );
 	} );
+} );
 
-	twic.requests.subscribe('getProfileFriendshipInfo', function(data, sendResponse) {
-		twic.twitter.getFriendshipInfo( data['source_id'], data['target_id'], function(friend) {
-			sendResponse( friend.fields );
-		} );
+twic.requests.subscribe('getProfileFriendshipInfo', function(data, sendResponse) {
+	twic.twitter.getFriendshipInfo( data['source_id'], data['target_id'], function(friend) {
+		sendResponse( friend.fields );
 	} );
-
-}() );
+} );
--- src/js/background/handlers/timeline.js
+++ src/js/background/handlers/timeline.js
@@ -5,104 +5,100 @@
  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  */
 
-( function() {
+// listen for the "getTimeline" request
+twic.requests.subscribe('getTimeline', function(data, sendResponse) {
+	// check for id in request data
+	if (!data['id']) {
+		sendResponse({ });
+		return;
+	}
 
-	// listen for the "getTimeline" request
-	twic.requests.subscribe('getTimeline', function(data, sendResponse) {
-		// check for id in request data
-		if (!data['id']) {
-			sendResponse({ });
-			return;
-		}
+	var
+		id = data['id'],
+		afterId = data['after'],
+		account = twic.accounts.getInfo(id);
 
+	// prepare tweets data and send the response
+	var replyWithTimeline = function(tweets, users) {
 		var
-			id = data['id'],
-			afterId = data['after'],
-			account = twic.accounts.getInfo(id);
+			reply = { },
+			tweetId;
 
-		// prepare tweets data and send the response
-		var replyWithTimeline = function(tweets, users) {
+		for (tweetId in tweets.items) {
 			var
-				reply = { },
-				tweetId;
+				tweet     = tweets.items[tweetId],
+				user      = users.items[tweet.fields['user_id']],
+				retweeted = tweet.fields['retweeted_user_id'] ? users.items[tweet.fields['retweeted_user_id']] : null;
 
-			for (tweetId in tweets.items) {
-				var
-					tweet     = tweets.items[tweetId],
-					user      = users.items[tweet.fields['user_id']],
-					retweeted = tweet.fields['retweeted_user_id'] ? users.items[tweet.fields['retweeted_user_id']] : null;
-
-				reply[tweet.fields['id']] = {
-					'msg': tweet.fields['msg'],
-					'user': user.getPart(['id', 'screen_name', 'avatar']),
-					'retweeted': retweeted ? retweeted.getPart(['id', 'screen_name', 'avatar']) : null
-				};
-			}
-
-			sendResponse( {
-				'account': {
-					'id': account.fields['id'],
-					'name': account.user.fields['screen_name']
-				},
-				'data': reply
-			} );
-		};
-
-		if (account) {
-			// we need to get the homeTimeline if user is in out accounts
-			twic.twitter.getHomeTimeline(id, replyWithTimeline, afterId);
-
-			account.setValue('unread_tweets_count', 0);
-			account.save();
-		}
-	} );
-
-	twic.requests.subscribe('sendTweet', function(data, sendResponse) {
-		// check for id in request data
-		if (!data['id']) {
-			sendResponse({ });
-			return;
+			reply[tweet.fields['id']] = {
+				'msg': tweet.fields['msg'],
+				'user': user.getPart(['id', 'screen_name', 'avatar']),
+				'retweeted': retweeted ? retweeted.getPart(['id', 'screen_name', 'avatar']) : null
+			};
 		}
 
-		var
-			id = data['id'],
-			tweet = data['tweet'];
-
-		twic.twitter.updateStatus(id, tweet, function() {
-			sendResponse({ });
+		sendResponse( {
+			'account': {
+				'id': account.fields['id'],
+				'name': account.user.fields['screen_name']
+			},
+			'data': reply
 		} );
+	};
+
+	if (account) {
+		// we need to get the homeTimeline if user is in out accounts
+		twic.twitter.getHomeTimeline(id, replyWithTimeline, afterId);
+
+		account.setValue('unread_tweets_count', 0);
+		account.save();
+	}
+} );
+
+twic.requests.subscribe('sendTweet', function(data, sendResponse) {
+	// check for id in request data
+	if (!data['id']) {
+		sendResponse({ });
+		return;
+	}
+
+	var
+		id = data['id'],
+		tweet = data['tweet'];
+
+	twic.twitter.updateStatus(id, tweet, function() {
+		sendResponse({ });
 	} );
+} );
 
-	twic.requests.subscribe('retweet', function(data, sendResponse) {
-		// check for id in request data
-		if (!data['userId']) {
-			sendResponse({ });
-			return;
-		}
+twic.requests.subscribe('retweet', function(data, sendResponse) {
+	// check for id in request data
+	if (!data['userId']) {
+		sendResponse({ });
+		return;
+	}
 
-		var
-			userId = data['userId'],
-			tweetId = data['tweetId'];
+	var
+		userId = data['userId'],
+		tweetId = data['tweetId'];
 
-		twic.twitter.retweet(userId, tweetId, function() {
-			sendResponse({ });
-		} );
+	twic.twitter.retweet(userId, tweetId, function() {
+		sendResponse({ });
 	} );
+} );
 
-	twic.requests.subscribe('delete', function(data, sendResponse) {
-		// check for id in request data
-		if (!data['userId']) {
-			sendResponse({ });
-			return;
-		}
+twic.requests.subscribe('delete', function(data, sendResponse) {
+	// check for id in request data
+	if (!data['userId']) {
+		sendResponse({ });
+		return;
+	}
 
-		var
-			userId = data['userId'],
-			tweetId = data['tweetId'];
+	var
+		userId = data['userId'],
+		tweetId = data['tweetId'];
 
-		twic.twitter.deleteTweet(userId, tweetId, function() {
-			sendResponse({ });
-		} );
+	twic.twitter.deleteTweet(userId, tweetId, function() {
+		sendResponse({ });
 	} );
-
-}() );
+} );
