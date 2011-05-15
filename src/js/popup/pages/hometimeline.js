ad3e423863d4fb7bd125786296b68788e77bd875
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-05-15 23:18:57 +0400

    O.o

--- src/js/popup/pages/hometimeline.js
+++ src/js/popup/pages/hometimeline.js
@@ -77,14 +77,16 @@
 		}
 
 		timeline.endUpdate();
-
+		
 		// fixme ugly bugfix to recalculate the max page height
-		setTimeout( function() {
-			page.style.maxHeight = Math.min(
-				screen.availHeight - window.screenTop - 100,
-				window.outerHeight > 100 ? window.outerHeight : 65536
-			) + 'px';
-		}, 100 );
+		//setTimeout( function() {
+			page.style.maxHeight = //Math.min(
+				screen.availHeight - window.screenTop - 100
+				//,
+				//window.outerHeight > 100 ? window.outerHeight : 65536
+			//)
+			 + 'px';
+		//}, 100 );
 	};
 
 	var update = function() {
