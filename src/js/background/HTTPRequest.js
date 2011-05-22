5bd62ab427ad0b78c87b676de0c8ad817b7164f8
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-05-23 00:40:49 +0400

    better regexp replace for request

--- src/js/background/HTTPRequest.js
+++ src/js/background/HTTPRequest.js
@@ -64,7 +64,7 @@ twic.HTTPRequest.prototype.encodeString = function(str) {
 	return encodeURIComponent(str)
 		.replace(/\!/g, '%21')
 		.replace(/\*/g, '%2A')
-		.replace(/\'/g, '%27')
+		.replace(/'/g, '%27')
 		.replace(/\(/g, '%28')
 		.replace(/\)/g, '%29');
 };
