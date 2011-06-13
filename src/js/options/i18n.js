3190ea658ac17c0b18098f50ea26effd55f844ca
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-06-13 17:34:36 +0400

    options - title

--- src/js/options/i18n.js
+++ src/js/options/i18n.js
@@ -7,11 +7,7 @@
 
 ( function() {
 
-	var
-		trTitle = twic.utils.lang.translate('options_title');
-
-	twic.dom.findElement('title').innerText = trTitle;
-	twic.dom.findElement('h1').innerText = trTitle;
+	twic.dom.findElement('title').innerText = twic.utils.lang.translate('options_title');
 	
 	twic.dom.findElement('#tabs li[data-content=ui] span').innerText = twic.utils.lang.translate('options_ui_title');
 	twic.dom.findElement('span.desc[data-key=avatar_size]').innerText = twic.utils.lang.translate('options_ui_avsize_title');
