ba44b228559e9ce2acb5f01529be3d343bf843f0
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-07-12 23:58:21 +0400

    suggest background page support

---
+++ src/js/background/handlers/suggest.js
@@ -0,0 +1,79 @@
+/**
+ * Suggest support
+ *
+ * Kalashnikov Igor <igor.kalashnikov@gmail.com>
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ */
+
+( function() {
+
+	var
+		/** @const **/ cacheTime = 10 * 60 * 1000,
+		cache = { };
+
+	// cleanup for suggest cache every cacheTime
+	setInterval( function() {
+		var
+			now = twic.utils.date.getCurrentTimestamp(),
+			part;
+
+		twic.debug.info('Suggest cleanup');
+
+		for (part in cache) {
+			if (now - cache[part].dt > cacheTime / 1000) {
+				delete cache[part];
+			}
+		}
+	}, cacheTime );
+
+	/**
+	 * Get the nick suggest list
+	 * @param {string} part Nick start part
+	 * @param {function(Array)} callback Callback with result
+	 */
+	var getNickSuggestList = function(part, callback) {
+		var
+			now = twic.utils.date.getCurrentTimestamp();
+
+		if (part in cache) {
+			cache[part].dt = now;
+			callback( cache[part].result );
+			return true;
+		}
+
+		twic.db.openQuery(
+			'select screen_name, screen_name_lower from users where screen_name_lower like ? limit 5',
+			[part + '%'],
+			/**
+			 * @this {SQLResultSetRowList}
+			 */
+			function() {
+				var
+					rows = this,
+					result = [],
+					i;
+
+				for (i = 0; i < rows.length; ++i) {
+					result.push(rows.item(i));
+				}
+
+				cache[part] = {
+					dt: now,
+					result: result
+				};
+
+				callback( result );
+			}
+		);
+	};
+
+	twic.requests.subscribe('getNickSuggest', function(data, sendResponse) {
+		var
+			/** @var {string} **/ nickPart = data['nickPart'];
+
+		getNickSuggestList( nickPart, function( list ) {
+			sendResponse( list );
+		} );
+	} );
+
+}() );
