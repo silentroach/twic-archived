a4a96f6cd00387cfaaa2ef4e5939e7936e1d6c8b
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-07-14 03:46:57 +0400

    partly object is really much better for gcc

--- src/js/requests.js
+++ src/js/requests.js
@@ -5,72 +5,71 @@
  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  */
 
-twic.requests = ( function() {
+twic.requests = { };
 
+/**
+ * @type {Object.<string, Array>}
+ * @private
+ */
+twic.requests.subscriptions_ = { };
+
+/**
+ * Send data to background
+ * @param {string} method Method name
+ * @param {Object} data Data to send
+ * @param {function(*)=} callback Callback function
+ */
+twic.requests.makeRequest = function(method, data, callback) {
+	chrome.extension.sendRequest( {
+		'method': method,
+		'data': data
+	}, function(reply) {
+		if (callback) {
+			callback(reply);
+		}
+	} );
+};
+
+/**
+ * Subscribe to the event
+ * @param {string} event Event
+ * @param {function(Object, function(Object))} callback Callback function
+ */
+twic.requests.subscribe = function(event, callback) {
+	if (!twic.requests.subscriptions_[event]) {
+		twic.requests.subscriptions_[event] = [];
+	}
+
+	twic.debug.info('subscribe for ' + event);
+
+	twic.requests.subscriptions_[event].push(callback);
+};
+
+twic.requests.handle = function(request, sender, sendResponse) {
 	var
-		self = { },
-		subscriptions = { };
+		method = request['method'],
+		subscription = twic.requests.subscriptions_[method],
+		data = request['data'] || {},
+		i;
 
-	/**
-	 * Send data to background
-	 * @param {string} method Method name
-	 * @param {Object} data Data to send
-	 * @param {function(Object)} callback Callback function
-	 */
-	self.makeRequest = function(method, data, callback) {
-		chrome.extension.sendRequest( {
-			'method': method,
-			'data': data
-		}, function(reply) {
-			if (callback) {
-				callback(reply);
-			}
-		} );
-	};
+	if (
+		method
+		&& subscription
+	) {
+		twic.debug.groupCollapsed('request ' + method + ' received');
+		twic.debug.dir(data);
+		twic.debug.groupEnd();
 
-	/**
-	 * Subscribe to the event
-	 * @param {string} event Event
-	 * @param {function(Object, function(Object))} callback Callback function
-	 */
-	self.subscribe = function(event, callback) {
-		if (!subscriptions[event]) {
-			subscriptions[event] = [];
+		for (i = 0; i < subscription.length; ++i) {
+			subscription[i](data, sendResponse);
 		}
+	} else {
+		sendResponse({});
 
-		twic.debug.info('subscribe for ' + event);
+		twic.debug.groupCollapsed('request received');
+		twic.debug.error('failed or handler not found');
+		twic.debug.dir(request);
+		twic.debug.groupEnd();
+	}
+};
 
-		subscriptions[event].push(callback);
-	};
-
-	self.handle = function(request, sender, sendResponse) {
-		var
-			method = request['method'],
-			subscription = subscriptions[method],
-			data = request['data'] || {},
-			i;
-
-		if (
-			method
-			&& subscription
-		) {
-			twic.debug.groupCollapsed('request ' + method + ' received');
-			twic.debug.dir(data);
-			twic.debug.groupEnd();
-
-			for (i = 0; i < subscription.length; ++i) {
-				subscription[i](data, sendResponse);
-			}
-		} else {
-			sendResponse({});
-
-			twic.debug.groupCollapsed('request received');
-			twic.debug.error('failed or handler not found');
-			twic.debug.dir(request);
-			twic.debug.groupEnd();
-		}
-	};
-
-	return self;
-
-}() );
