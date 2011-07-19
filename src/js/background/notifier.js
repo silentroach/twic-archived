d8377e7147ccb71a7662a28dcfa0607264d46fc3
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-07-20 01:53:23 +0400

    notifier draft object (refs #35)

---
+++ src/js/background/notifier.js
@@ -0,0 +1,31 @@
+/**
+ * Notifications
+ *
+ * Kalashnikov Igor <igor.kalashnikov@gmail.com>
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ */
+ 
+twic.notifier = { };
+
+/**
+ * @param {string} title Title
+ * @param {string} description Description
+ */
+twic.notifier.show = function(title, description) {
+	var
+		notification = webkitNotifications.createNotification(
+			'/img/icons/48.png',
+			title,
+			description
+		);
+		
+	notification.show();
+	
+	// hide it after 5 seconds
+	
+	setTimeout( function() {
+		if (notification) {
+			notification.cancel();
+		}
+	}, 5000 );
+};
