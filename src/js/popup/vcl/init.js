f9c1556667b15c95d0e7abbb91a2e45821dd0f39
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-08-12 02:37:40 +0400

    vcl

---
+++ src/js/popup/vcl/init.js
@@ -0,0 +1,9 @@
+/**
+ * Init script for popup
+ *
+ * Kalashnikov Igor <igor.kalashnikov@gmail.com>
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ */
+
+// namespace for visual components
+twic.vcl = { };
