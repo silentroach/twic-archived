be37821d945f155b1cea9824199e4c782631b7fb
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-08-21 03:05:18 +0400

    each platform now have separate font settings

---
+++ src/js/popup/platform.js
@@ -0,0 +1,29 @@
+/**
+ * Platform detector
+ *
+ * Kalashnikov Igor <igor.kalashnikov@gmail.com>
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ */
+
+/**
+ * Platforms
+ * @enum {string}
+ */
+twic.platforms = {
+	WINDOWS: 'windows',
+	OSX: 'osx',
+	LINUX: 'linux'
+};
+
+/**
+ * @type {twic.platforms}
+ */
+twic.platform = twic.platforms.WINDOWS;
+
+if (navigator.appVersion.indexOf('Mac') >= 0) {
+	twic.platform = twic.platforms.OSX;
+} else {
+	twic.platform = twic.platforms.LINUX;
+}
+
+document.body.classList.add(twic.platform);
