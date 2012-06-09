ca2c0f1a314d4c9b24564f928c08902f45b3bb4d
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2012-06-09 04:02:28 +0400

    async lazy link attach ability check

--- src/js/popup/vcl/TweetEditor.js
+++ src/js/popup/vcl/TweetEditor.js
@@ -20,28 +20,66 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
         /** @type {Storage} **/ storage         = window.localStorage,
         /** @type {Element} **/ editorWrapper   = twic.dom.expandElement('div.tweetEditor'),
         /** @type {Element} **/ editorSend      = twic.dom.expandElement('input'),
-        /** @type {Element} **/ editorAttach    = twic.dom.expandElement('img.attach'),
+        /** @type {Element} **/ editorAttach    = twic.dom.expandElement('img.attach.disabled'),
         /** @type {Element} **/ rightButtons    = twic.dom.expandElement('div.rb'),
         /** @type {Element} **/ editorCounter   = twic.dom.expandElement('span'),
-        /** @type {Element} **/ clearer         = twic.dom.expandElement('div.clearer'),
-        /** @type {number}  **/ charCount       = 0;
+        /** @type {Element} **/ clearer         = twic.dom.expandElement('div.clearer');
 
     /** @type {boolean} **/ editor.autoRemovable = false;
 
     /**
      * @type {string}
+     * @private
      */
     this.constStartVal_ = '';
 
     /**
+     * @type {number}
+     * @private
+     */
+    editor.charCount_ = 0;
+
+    /**
+     * @type {Element}
      * @private
      */
     this.suggestBlock_ = twic.dom.expandElement('div');
 
     /**
+     * @type {Element}
      * @private
      */
     this.editorWrapper_ = editorWrapper;
+
+    /**
+     * @type {number}
+     * @private
+     */
+    editor.userId_ = userId;
+
+    /**
+     * @type {string=}
+     * @private
+     */
+    editor.replyTo_ = replyTo;
+
+    /**
+     * @type {Element}
+     * @private
+     */
+    editor.buttonSend_ = editorSend;
+
+    /**
+     * @type {Element}
+     * @private
+     */
+    editor.buttonAttach_ = editorAttach;
+
+    /**
+     * @type {Boolean}
+     * @private
+     */
+    editor.isAttachAbilityChecked_ = false;
 
     /**
      * @type {Element}
@@ -50,7 +88,15 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
     this.editorTextarea_ = twic.dom.expandElement('textarea');
 
     this.editorTextarea_['spellcheck'] = false;
-    this.editorTextarea_.placeholder = twic.utils.lang.translate(replyTo ? 'placeholder_tweet_reply' : 'placeholder_tweet_new');
+    this.editorTextarea_.placeholder = twic.utils.lang.translate(
+        replyTo ? 'placeholder_tweet_reply' : 'placeholder_tweet_new'
+    );
+
+    /**
+     * @type {Element}
+     * @private
+     */
+    this.editorCounter_ = editorCounter;
 
     /**
      * @private
@@ -69,22 +115,19 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
     this.geoInfo_ = twic.dom.expandElement('img.geo.disabled');
     // @resource img/buttons/map.png
     this.geoInfo_.src = 'img/buttons/map.png';
-    this.geoInfo_.title = twic.utils.lang.translate('title_button_geo') + ' - ' + twic.utils.lang.translate('disabled');
+    this.geoInfo_.title = twic.utils.lang.translate('title_button_geo') +
+        ' - ' + twic.utils.lang.translate('disabled');
 
     // @resource img/buttons/link.png
     editorAttach.src = 'img/buttons/link.png';
-
-    if (!twic.vcl.TweetEditor.prototype.currentURL_) {
-        editorAttach.title = twic.utils.lang.translate('title_attach_link_disabled');
-
-        editorAttach.classList.add('disabled');
-    } else {
-        editorAttach.title = twic.utils.lang.translate('title_attach_link' + (twic.platforms.OSX === twic.platform ? '_osx' : ''));
-    }
+    editorAttach.title = twic.utils.lang.translate('title_attach_link_disabled');
 
     editorSend.type  = 'button';
     editorSend.value = twic.utils.lang.translate(replyTo ? 'button_reply' : 'button_send');
-    editorSend.title = twic.utils.lang.translate('title_button_send' + (twic.platforms.OSX === twic.platform ? '_osx' : ''));
+    editorSend.title = twic.utils.lang.translate(
+        'title_button_send' +
+        (twic.platforms.OSX === twic.platform ? '_osx' : '')
+    );
 
     rightButtons.appendChild(this.geoInfo_);
     rightButtons.appendChild(editorAttach);
@@ -106,47 +149,6 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
     this.geoInfo_.addEventListener('click', function() {
         editor.toggleMap_.call(editor);
     }, false );
-
-    var checkTweetArea = function() {
-        var val = editor.editorTextarea_.value;
-
-        if (editorWrapper.classList.contains(twic.vcl.TweetEditor.sendingClass)) {
-            return;
-        }
-
-        if (val.substring(0, editor.constStartVal_.length) !== editor.constStartVal_) {
-            editor.editorTextarea_.value = editor.constStartVal_;
-            editor.editorTextarea_.selectionStart = editor.editorTextarea_.selectionEnd = editor.constStartVal_.length;
-            val = editor.constStartVal_;
-        }
-
-        charCount = editor.getCharCount_();
-
-        while (
-            editor.editorTextarea_.rows > 1
-            && editor.editorTextarea_.scrollHeight <= editor.editorTextarea_.offsetHeight
-        ) {
-            --editor.editorTextarea_.rows;
-        }
-
-        while (editor.editorTextarea_.scrollHeight > editor.editorTextarea_.offsetHeight) {
-            ++editor.editorTextarea_.rows;
-        }
-
-        editorCounter.innerHTML = (twic.vcl.TweetEditor.MAXCOUNT - charCount).toString();
-
-        if (charCount > twic.vcl.TweetEditor.MAXCOUNT) {
-            editorWrapper.classList.add(twic.vcl.TweetEditor.overloadClass);
-        } else {
-            editorWrapper.classList.remove(twic.vcl.TweetEditor.overloadClass);
-        }
-
-        editorSend.disabled = (
-            charCount === 0
-            || charCount > twic.vcl.TweetEditor.MAXCOUNT
-            || val === editor.constStartVal_
-        );
-    };
 
     /**
      * Try to send the tweet
@@ -181,79 +183,14 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
         } );
     };
 
-    /**
-     * Function to get the path to localStorage element
-     * @return {string}
-     */
-    var getStoragePath = function() {
-        // todo append the tweet id if editor is for composing the reply
-        return 'tweetEditor_' + userId + (replyTo ? '_' + replyTo : '');
-    };
-
-    var keyUpHandler = function() {
-        var
-            val  = editor.editorTextarea_.value,
-            path = getStoragePath();
-
-        checkTweetArea();
-
-        if (val === '' ||
-            val === editor.constStartVal_.trim() ||
-            val.length < editor.constStartVal_.length
-        ) {
-            storage.removeItem(path);
-        } else {
-            storage.setItem(path, val);
-        }
-    };
-
     // store the textarea value on each keyup to avoid data loss on popup close
     this.editorTextarea_.addEventListener('keyup', function(e) {
         if (13 === e.keyCode) {
             return false;
         }
 
-        keyUpHandler();
+        editor.keyUpHandler_();
     }, false );
-
-    if (twic.vcl.TweetEditor.prototype.currentURL_) {
-        editorAttach.addEventListener('click', function(e) {
-            var
-                url = twic.vcl.TweetEditor.prototype.currentURL_,
-                title = twic.vcl.TweetEditor.prototype.currentTitle_,
-                selStart = editor.editorTextarea_.selectionStart,
-                selEnd = editor.editorTextarea_.selectionEnd,
-                newVal = editor.editorTextarea_.value.substr(0, selStart);
-
-            if (
-                newVal.length > 0
-                && ' ' !== newVal.substr(-1)
-            ) {
-                newVal += ' ';
-            }
-
-            newVal += url + (
-                twic.events.isEventWithModifier(e) && '' !== title ? ' ' + title : ''
-            );
-
-            if (' ' !== editor.editorTextarea_.value.substr(selEnd).substr(0, 1)) {
-                newVal += ' ';
-            }
-
-            selStart = newVal.length;
-
-            newVal += editor.editorTextarea_.value.substr(selEnd);
-
-            editor.editorTextarea_.value = newVal;
-
-            editor.editorTextarea_.selectionStart = selStart;
-            editor.editorTextarea_.selectionEnd = selStart;
-
-            editor.editorTextarea_.focus();
-
-            keyUpHandler();
-        }, false );
-    }
 
     // prevent user to press enter
     this.editorTextarea_.addEventListener('keydown', function(e) {
@@ -263,8 +200,7 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
                 // do not allow to remove the constant part
                 var constStartValLength = editor.constStartVal_.length;
 
-                if (
-                    constStartValLength > 0
+                if (constStartValLength > 0
                     && editor.editorTextarea_.value.length <= constStartValLength
                 ) {
                     e.preventDefault();
@@ -278,9 +214,8 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
                 if (twic.events.isEventWithModifier(e)) {
                     e.stopPropagation();
 
-                    if (
-                        charCount > 0
-                        && charCount < 141
+                    if (editor.charCount_ > 0
+                        && editor.charCount_ < 141
                     ) {
                         tryToSend();
                     }
@@ -299,8 +234,13 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
     };
 
     this.editorTextarea_.addEventListener('focus', function(e) {
+        if (!editor.isAttachAbilityChecked_) {
+            editor.isAttachAbilityChecked_ = true;
+            editor.checkLinkAttachAbility_();
+        }
+
         editorWrapper.classList.add(twic.vcl.TweetEditor.focusedClass);
-        checkTweetArea();
+        editor.checkTweetArea_();
 
         editor.onFocus();
     }, false );
@@ -326,12 +266,12 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
 
     reset();
 
-    var backupText = storage.getItem(getStoragePath());
+    var backupText = storage.getItem(editor.getBackupStoragePath_());
     if (backupText) {
         this.editorTextarea_.value = backupText;
         // timeout to resize it after ui is loaded
         setTimeout( function() {
-            checkTweetArea();
+            editor.checkTweetArea_();
         }, 200);
     }
 
@@ -339,7 +279,7 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
 
     editor.reset = function() {
         // empty the localstorage backup
-        storage.removeItem(getStoragePath());
+        storage.removeItem(editor.getBackupStoragePath_());
 
         reset();
     };
@@ -354,7 +294,7 @@ twic.vcl.TweetEditor = function(userId, parent, replyTo) {
 
     var suggest = new twic.vcl.Suggest(this);
     suggest.onSelect = function() {
-        checkTweetArea();
+        editor.checkTweetArea_();
     };
 };
 
@@ -364,38 +304,25 @@ twic.vcl.TweetEditor.options = {
 };
 
 /**
+ * Current tab url is already checked
+ * @type {Boolean}
+ * @private
+ */
+twic.vcl.TweetEditor.prototype.currentTabChecked_ = false;
+
+/**
  * Current url to paste it into the tweet
+ * @type {Boolean}
  * @private
  */
 twic.vcl.TweetEditor.prototype.currentURL_ = false;
 
 /**
  * Current title to paste it into the tweet
+ * @type {Boolean}
  * @private
  */
 twic.vcl.TweetEditor.prototype.currentTitle_ = '';
-
-chrome.tabs.getSelected(null, function(tab) {
-    if (tab) {
-        var
-            url = tab.url.trim(),
-            title = tab.title.trim();
-
-        if (
-            url.length > 4
-            && ('http' === url.substr(0, 4)
-                || 'ftp' === url.substr(0, 3)
-            )
-        ) {
-            if ('/' === url.substr(-1)) {
-                url = url.substring(0, url.length - 1);
-            }
-
-            twic.vcl.TweetEditor.prototype.currentURL_ = url;
-            twic.vcl.TweetEditor.prototype.currentTitle_ = title;
-        }
-    }
-} );
 
 // ------------------------------------------
 
@@ -403,6 +330,180 @@ chrome.tabs.getSelected(null, function(tab) {
 /** @const **/ twic.vcl.TweetEditor.focusedClass  = 'focused';
 /** @const **/ twic.vcl.TweetEditor.sendingClass  = 'sending';
 /** @const **/ twic.vcl.TweetEditor.MAXCOUNT = 140;
+
+/**
+ * Check the ability to attach link to tweet
+ * @private
+ */
+twic.vcl.TweetEditor.prototype.checkLinkAttachAbility_ = function() {
+    var
+        editor = this;
+
+    var doAssignAttachEvent = function() {
+        editor.buttonAttach_.title = twic.utils.lang.translate(
+            'title_attach_link' +
+            (twic.platforms.OSX === twic.platform ? '_osx' : '')
+        );
+
+        editor.buttonAttach_.addEventListener('click', function(e) {
+            editor.onInsertCurrentLink_.call(editor, e);
+        }, false);
+
+        twic.dom.removeClass(editor.buttonAttach_, 'disabled');
+    };
+
+    if (!twic.vcl.TweetEditor.prototype.currentTabChecked_) {
+        twic.vcl.TweetEditor.prototype.currentTabChecked_ = true;
+
+        chrome.tabs.getSelected(null, function(tab) {
+            if (tab) {
+                var
+                    url = tab.url.trim(),
+                    title = tab.title.trim();
+
+                if (url.length > 7
+                    && ('http' === url.substr(0, 4)
+                        || 'ftp' === url.substr(0, 3)
+                    )
+                ) {
+                    if ('/' === url.substr(-1)) {
+                        url = url.substring(0, url.length - 1);
+                    }
+
+                    twic.vcl.TweetEditor.prototype.currentURL_ = url;
+                    twic.vcl.TweetEditor.prototype.currentTitle_ = title;
+
+                    doAssignAttachEvent();
+                }
+            }
+        } );
+    } else
+    if (twic.vcl.TweetEditor.prototype.currentURL_) {
+        doAssignAttachEvent();
+    }
+};
+
+/**
+ * Check the tweet editor area
+ * @private
+ */
+twic.vcl.TweetEditor.prototype.checkTweetArea_ = function() {
+    var
+        editor = this,
+        val = editor.editorTextarea_.value;
+
+    if (editor.editorWrapper_.classList.contains(twic.vcl.TweetEditor.sendingClass)) {
+        return;
+    }
+
+    if (val.substring(0, editor.constStartVal_.length) !== editor.constStartVal_) {
+        editor.editorTextarea_.value = editor.constStartVal_;
+        editor.editorTextarea_.selectionStart = editor.editorTextarea_.selectionEnd = editor.constStartVal_.length;
+        val = editor.constStartVal_;
+    }
+
+    editor.charCount_ = editor.getCharCount_();
+
+    while (editor.editorTextarea_.rows > 1
+        && editor.editorTextarea_.scrollHeight <= editor.editorTextarea_.offsetHeight
+    ) {
+        --editor.editorTextarea_.rows;
+    }
+
+    while (editor.editorTextarea_.scrollHeight > editor.editorTextarea_.offsetHeight) {
+        ++editor.editorTextarea_.rows;
+    }
+
+    editor.editorCounter_.innerHTML = (twic.vcl.TweetEditor.MAXCOUNT - editor.charCount_).toString();
+
+    if (editor.charCount_ > twic.vcl.TweetEditor.MAXCOUNT) {
+        editor.editorWrapper_.classList.add(twic.vcl.TweetEditor.overloadClass);
+    } else {
+        editor.editorWrapper_.classList.remove(twic.vcl.TweetEditor.overloadClass);
+    }
+
+    editor.buttonSend_.disabled = (
+        editor.charCount_ === 0
+        || editor.charCount_ > twic.vcl.TweetEditor.MAXCOUNT
+        || val === editor.constStartVal_
+    );
+};
+
+/**
+ * Function to get the path to localStorage element
+ * @return {string}
+ */
+twic.vcl.TweetEditor.prototype.getBackupStoragePath_ = function() {
+    var
+        editor = this;
+
+    return 'tweetEditor_' + editor.userId_ +
+        (editor.replyTo_ ? '_' + editor.replyTo_ : '');
+};
+
+/**
+ * Insert current tab link into editor area
+ * @private
+ */
+twic.vcl.TweetEditor.prototype.onInsertCurrentLink_ = function(e) {
+    var
+        editor = this,
+        url = twic.vcl.TweetEditor.prototype.currentURL_,
+        title = twic.vcl.TweetEditor.prototype.currentTitle_,
+        selStart = editor.editorTextarea_.selectionStart,
+        selEnd = editor.editorTextarea_.selectionEnd,
+        newVal = editor.editorTextarea_.value.substr(0, selStart);
+
+    if (newVal.length > 0
+        && ' ' !== newVal.substr(-1)
+    ) {
+        newVal += ' ';
+    }
+
+    newVal += url + (
+        twic.events.isEventWithModifier(e) && '' !== title ? ' ' + title : ''
+    );
+
+    if (' ' !== editor.editorTextarea_.value.substr(selEnd).substr(0, 1)) {
+        newVal += ' ';
+    }
+
+    selStart = newVal.length;
+
+    newVal += editor.editorTextarea_.value.substr(selEnd);
+
+    editor.editorTextarea_.value = newVal;
+
+    editor.editorTextarea_.selectionStart = selStart;
+    editor.editorTextarea_.selectionEnd = selStart;
+
+    editor.editorTextarea_.focus();
+
+    editor.keyUpHandler_();
+};
+
+/**
+ * Key up handler
+ * @private
+ */
+twic.vcl.TweetEditor.prototype.keyUpHandler_ = function() {
+    var
+        editor = this,
+        val  = editor.editorTextarea_.value,
+        storage = window.localStorage,
+        path = editor.getBackupStoragePath_();
+
+    editor.checkTweetArea_();
+
+    if (val === '' ||
+        val === editor.constStartVal_.trim() ||
+        val.length < editor.constStartVal_.length
+    ) {
+        storage.removeItem(path);
+    } else {
+        storage.setItem(path, val);
+    }
+};
 
 /**
  * @param {boolean=} setstart Set the cursor to the start
@@ -558,7 +659,9 @@ twic.vcl.TweetEditor.prototype.getTextarea = function() {
  * @param {string=} replyTo Reply to tweet
  * @param {function()=} callback Callback for reset
  */
-twic.vcl.TweetEditor.prototype.onTweetSend = function(editor, tweet, replyTo, callback) { };
+twic.vcl.TweetEditor.prototype.onTweetSend = function(editor, tweet, replyTo, callback) {
+    callback();
+};
 
 /**
  * Handler for the focus event
