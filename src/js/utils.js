d541ec24aa75a18b254d277d885b9592eb959856
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2012-07-11 23:53:48 +0400

    favicons api is working for now

--- src/js/utils.js
+++ src/js/utils.js
@@ -117,8 +117,7 @@ twic.utils.url.humanize = function(url, lnks) {
 
     if (faviconUrl) {
         classes = ' class="aicon"';
-        //cutted = '<img src="chrome://favicon/' + faviconUrl + '" />';
-        cutted = '<img src="' + faviconUrl + '/favicon.ico" />';
+        cutted = '<img src="chrome://favicon/' + faviconUrl + '" />';
     } else
     if (clen > 30) {
         cutted = cutted.substring(0, 30) + '&hellip;';
